deb5d94853b20dbdc073bbd2e157b8c3b49d29bc
Alan Dyer <dyer.alan@gmail.com>
2013-12-05 19:12:16 -0400

    Adds core function CheckWin and a unit test

--- lib/numbermind.ex
+++ lib/numbermind.ex
@@ -1,2 +1,33 @@
 defmodule Numbermind do
+
+def checkwin(guess, answer) do
+  checkwin(guess, answer, 1, [])
+
 end
+
+def checkwin([], answer, digit, acc) do
+  Enum.reverse(acc)
+end
+def checkwin([guess | rest], answer, digit, acc) do
+  cond do
+    guess == Enum.at(answer, digit - 1) ->
+      checkwin(rest, answer, digit + 1, ["X" | acc])
+    elementof(guess, answer) ->
+      checkwin(rest, answer, digit + 1, ["O" | acc])
+    true ->
+      checkwin(rest, answer, digit + 1, ["-" | acc])
+  end
+end
+
+def elementof(a, []) do
+  :false
+end
+def elementof(a, [headof | mylist]) do
+  if a == headof do
+    :true
+  else
+    elementof(a, mylist)
+  end
+end 
+
+end   
--- test/numbermind_test.exs
+++ test/numbermind_test.exs
@@ -4,4 +4,8 @@ defmodule NumbermindTest do
   test "the truth" do
     assert(true)
   end
+  
+  test "check win" do
+    assert  Numbermind.checkwin([1, 2, 3, 4], [3, 5, 6, 4]) == ["-", "-", "O", "X"]
+  end
 end
