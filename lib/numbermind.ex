82dc43b30c5e5bef068af21ad56df778f0f4f7f3
Alan Dyer <dyer.alan@gmail.com>
2013-12-10 19:23:52 -0400

    Adds a number generator; Refactors to use native functions in some cases

--- lib/numbermind.ex
+++ lib/numbermind.ex
@@ -1,33 +1,25 @@
 defmodule Numbermind do
 
-def checkwin(guess, answer) do
-  checkwin(guess, answer, 1, [])
-
-end
-
-def checkwin([], answer, digit, acc) do
-  Enum.reverse(acc)
-end
-def checkwin([guess | rest], answer, digit, acc) do
-  cond do
-    guess == Enum.at(answer, digit - 1) ->
-      checkwin(rest, answer, digit + 1, ["X" | acc])
-    elementof(guess, answer) ->
-      checkwin(rest, answer, digit + 1, ["O" | acc])
-    true ->
-      checkwin(rest, answer, digit + 1, ["-" | acc])
+  def checkwin(guess, answer) do
+    checkwin(guess, answer, 1, [])
   end
-end
 
-def elementof(a, []) do
-  :false
-end
-def elementof(a, [headof | mylist]) do
-  if a == headof do
-    :true
-  else
-    elementof(a, mylist)
+  def checkwin([], answer, digit, acc) do
+    Enum.reverse(acc)
   end
-end 
+  def checkwin([guess | rest], answer, digit, acc) do
+    cond do
+      guess == Enum.at(answer, digit - 1) ->
+        checkwin(rest, answer, digit + 1, ["X" | acc])
+      Enum.member?(answer, guess) ->
+        checkwin(rest, answer, digit + 1, ["O" | acc])
+      true ->
+        checkwin(rest, answer, digit + 1, ["-" | acc])
+    end
+  end
+
+  def getRandomAnswerList(size) do
+    Stream.repeatedly(fn -> :random.uniform(10) - 1 end) |> Enum.take size
+  end
 
 end   
--- test/numbermind_test.exs
+++ test/numbermind_test.exs
@@ -8,4 +8,12 @@ defmodule NumbermindTest do
   test "check win" do
     assert  Numbermind.checkwin([1, 2, 3, 4], [3, 5, 6, 4]) == ["-", "-", "O", "X"]
   end
+  
+  test "check getRandomAnswerList size" do
+    assert length(Numbermind.getRandomAnswerList(4)) == 4
+  end
+  
+  test "check randomness of getRandomAnswerList" do
+    assert Numbermind.getRandomAnswerList(1000) != Numbermind.getRandomAnswerList(1000)
+  end
 end
